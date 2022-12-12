1e05927b6f212d9e93bb41ded6513a40bb7b9df0
dusw <1030138554@qq.com>
2022-12-12 17:38:35 +0800

    ✨ feat: 更新3.4数据/修复卡牌说明

--- src/views/Genshin/Card.vue
+++ src/views/Genshin/Card.vue
@@ -2,7 +2,10 @@
 <n-layout>
     <n-layout-header class="header-top">
       <div>
-        <h2>「七圣召唤」卡牌（分类仅供参考）</h2>
+        <h2>「七圣召唤」卡牌（分类仅供参考，详情链接仅为拆包网）</h2>
+        <p>
+          <a href="https://bbs.mihoyo.com/ys/obc/channel/map/231/234">米游社七圣召唤wiki</a>
+        </p>
         <n-skeleton v-if="loading" :width="150" :sharp="false" size="medium" />
         <n-gradient-text v-else :gradient="{ from: 'rgb(85, 85, 85)', to: 'rgb(170, 170, 170)' }">
           共{{total}}个，已展示{{itemList.length}}个
